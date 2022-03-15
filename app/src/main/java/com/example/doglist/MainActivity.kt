1df7d743e47b9224255b37fd538a82563cb2124d
andreribeirodev <andreeleeandro@gmail.com>
2022-03-14 23:27:18 -0300

    App finalizado

---
+++ app/src/main/java/com/example/doglist/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.doglist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.doglist.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
+
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener,
+    android.widget.SearchView.OnQueryTextListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: DogAdapter
+    private val dogImages = mutableListOf<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.svDogs.setOnQueryTextListener(this)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        adapter = DogAdapter(dogImages)
+        binding.rvListDogs.layoutManager = LinearLayoutManager(this)
+        binding.rvListDogs.adapter = adapter
+    }
+
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://dog.ceo/api/breed/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun searchByName(raca: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val call: Response<DogResponse> = getRetrofit()
+                .create(APIService::class.java)
+                .getDogsByBreeds("$raca/images")
+            val puppies = call.body()
+
+            runOnUiThread {
+                if (call.isSuccessful) {
+                   val images = puppies?.images ?: emptyList()
+                    dogImages.clear()
+                    dogImages.addAll(images)
+                    adapter.notifyDataSetChanged()
+                } else {
+                    showError()
+                }
+            }
+
+        }
+    }
+
+    private fun showError() {
+        Toast.makeText(this, "Deu Errado !", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        if(!query.isNullOrEmpty()){
+            searchByName(query.lowercase(Locale.getDefault()))
+        }
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+       return true
+    }
+}
